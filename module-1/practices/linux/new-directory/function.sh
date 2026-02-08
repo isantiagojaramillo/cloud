c1260bc11162878c3732fd27eb2ab44d0ea9be40
Santiago Jaramillo <imsntiago@gmail.com>
2026-02-08 18:44:11 -0500

    first module

---
+++ module-1/practices/linux/new-directory/function.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for days in Lunes Martes Miércoles Jueves Viernes Sábado Domingo
+do
+echo “Day: $days”
+done
---
+++ module-1/practices/linux/new-directory/while-if.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+isvalid=true
+count=1
+while [ $isvalid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+((count++))
+done
